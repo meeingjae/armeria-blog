e9f15dae8ce6823b33d91419361e729573f9b3ca
Minjae Kim <minjae.kim@mytrum.com>
2022-06-01 08:25:19 +0900

    feat: Create Blog 부가 기능 구현
    Converter 및 Exception Handler 구현

---
+++ src/main/java/dto/BlogPostResponseConverter.java
@@ -0,0 +1,27 @@
+package dto;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.linecorp.armeria.common.HttpHeaders;
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.common.HttpStatus;
+import com.linecorp.armeria.common.MediaType;
+import com.linecorp.armeria.common.ResponseHeaders;
+import com.linecorp.armeria.common.annotation.Nullable;
+import com.linecorp.armeria.server.ServiceRequestContext;
+import com.linecorp.armeria.server.annotation.ResponseConverterFunction;
+
+public class BlogPostResponseConverter implements ResponseConverterFunction {
+    private final ObjectMapper mapper = new ObjectMapper();
+    @Override
+    public HttpResponse convertResponse(ServiceRequestContext ctx, ResponseHeaders headers,
+                                        @Nullable Object result, HttpHeaders trailers) throws Exception {
+        if(result instanceof BlogPost) {
+            return HttpResponse.of(HttpStatus.OK,
+                                   MediaType.JSON_UTF_8,
+                                   "%S",(mapper.writeValueAsString((BlogPost) result)),
+                                   trailers);
+        }
+        return ResponseConverterFunction.fallthrough();
+    }
+}
--- src/main/java/service/BlogService.java
+++ src/main/java/service/BlogService.java
@@ -5,20 +5,29 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import com.linecorp.armeria.common.HttpResponse;
 import com.linecorp.armeria.server.annotation.Description;
+import com.linecorp.armeria.server.annotation.ExceptionHandler;
 import com.linecorp.armeria.server.annotation.Post;
 import com.linecorp.armeria.server.annotation.RequestConverter;
+import com.linecorp.armeria.server.annotation.ResponseConverter;
 
 import dto.BlogPost;
 import dto.BlogPostConverter;
+import dto.BlogPostResponseConverter;
+import handler.BlogExceptionHandler;
 
 @Description("Doc Description???")
 public class BlogService {
     private final Map<Integer, BlogPost> blogPosts = new ConcurrentHashMap<>();
 
+    @ExceptionHandler(BlogExceptionHandler.class)
     @Post("/blogs")
     @RequestConverter(BlogPostConverter.class)
-    public HttpResponse createBlogPost(BlogPost blogPost) {
+    @ResponseConverter(BlogPostResponseConverter.class)
+    public BlogPost createBlogPost(BlogPost blogPost) { //RequestConverter
         blogPosts.put(blogPost.getId(), blogPost);
-        return HttpResponse.ofJson(blogPost);
+        if(blogPost.getTitle() == null){ // exception handler example
+            throw new IllegalArgumentException();
+        }
+        return blogPost; // ResponseConverter
     }
 }
