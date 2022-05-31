38039955adcc69e298d4e5c769c5d1b6cd25b189
Minjae Kim <minjae.kim@mytrum.com>
2022-05-31 15:07:58 +0900

    feat: Create Blog 기능 구현

--- src/main/java/service/BlogService.java
+++ src/main/java/service/BlogService.java
@@ -1,2 +1,24 @@
-package service;public class BlogService {
+package service;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.server.annotation.Description;
+import com.linecorp.armeria.server.annotation.Post;
+import com.linecorp.armeria.server.annotation.RequestConverter;
+
+import dto.BlogPost;
+import dto.BlogPostConverter;
+
+@Description("Doc Description???")
+public class BlogService {
+    private final Map<Integer, BlogPost> blogPosts = new ConcurrentHashMap<>();
+
+    @Post("/blogs")
+    @RequestConverter(BlogPostConverter.class)
+    public HttpResponse createBlogPost(BlogPost blogPost) {
+        blogPosts.put(blogPost.getId(), blogPost);
+        return HttpResponse.ofJson(blogPost);
+    }
 }
